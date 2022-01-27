c968837d460e4c70382b10721cf67493e43468da
anamerasul <anamerasul@gmail.com>
2022-01-27 20:19:36 +0600

    multiple condition

---
+++ multiple_condition.js
@@ -0,0 +1,36 @@
+var gotJob = true;
+
+var moneySaved =205000;
+
+var hasFlat =false;
+
+var hasHouse =false;
+
+// if(gotJob ==true && moneySaved>200000){
+//     console.log("biye hbe")
+// }
+
+// else{
+//     console.log("tur kopale biye nai")
+// }
+// if(gotJob ==true && moneySaved>200000 && hasFlat==true){
+//     console.log("biye hbe")
+// }
+
+// else{
+//     console.log("tur kopale biye nai")
+// }
+// if(gotJob ==true || moneySaved>200000 || hasFlat==true){
+//     console.log("biye hbe")
+// }
+
+// else{
+//     console.log("tur kopale biye nai")
+// }
+if((gotJob ==true && moneySaved>200000) || hasHouse==true){
+    console.log("biye hbe")
+}
+
+else{
+    console.log("tur kopale biye nai")
+}
