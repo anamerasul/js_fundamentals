48828605e3a7a3fff4e53a96bcb5f345828644dc
anamerasul <anamerasul@gmail.com>
2022-01-27 15:22:59 +0600

    array

---
+++ array.js
@@ -0,0 +1,23 @@
+// narmal variable
+var friendsAge=11;
+
+var friendsAgenew=[11, 13,17];
+
+var muriChananurFee =[34,55,221,5];
+
+var naiyaks =['mahi','opu','sabnur','kopila'];
+
+var oddNumer=[1,3,5,7,9];
+
+var vowels=['a','e','i','o','u'];
+
+var vowelsCount =vowels.length;
+
+
+
+console.log(friendsAgenew);
+
+console.log(vowels);
+
+console.log(vowels.length);
+console.log(vowelsCount);
