1d401cac904cdf312bdc56dd8534df4b794b49a0
anamerasul <anamerasul@gmail.com>
2022-01-27 19:38:56 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,16 @@
+// // console.log(5>6);
+// console.log(5<6);
+// console.log(5 == 6);
+// console.log(5 != 6);
+// console.log(6 != 6);
+
+
+var myLove =99;
+var herLove =100;
+// console.log(myLove>herLove);
+// console.log(myLove == herLove);
+// console.log(myLove < herLove);
+// console.log(myLove != herLove);
+
+console.log(studyGood && goodPerson);
+console.log(isBanker||goodResult);
--- push_pop.js
+++ push_pop.js
@@ -25,22 +25,23 @@ var pos = friendsAge.indexOf(11);
 
 
 console.log(pos);
-console.log(friendsAge.splice(2,3));
+console.log(friendsAge);
+console.log(friendsAge.splice(2,1));
 
-console.log(friendsAge.slice(0),1);
+console.log(friendsAge.slice(pos,2));
 
 // friendsAge.pop();
 // console.log(friendsAge);
 // friendsAge.pop();
 // friendsAge.pop();
-console.log(friendsAge);
+// console.log(friendsAge);
 // friendsAge.push(23);
 // console.log(friendsAge);
 // friendsAge.pop();
 // console.log(friendsAge)
 
-var lastItem =friendsAge.pop();
-console.log(lastItem)
+// var lastItem =friendsAge.pop();
+// console.log(lastItem)
 
 // friendsAge.pop();
 // friendsAge.pop();
@@ -49,32 +50,32 @@ console.log(lastItem)
 // console.log(friendsAge);
 
 // unshift add element array from first
-friendsAge.unshift(15);
-console.log(friendsAge);
+// friendsAge.unshift(15);
+// console.log(friendsAge);
 // delete element from frist using shift() in arry
-friendsAge.shift();
-console.log(friendsAge);
-friendsAge.shift();
-console.log(friendsAge);
+// friendsAge.shift();
+// console.log(friendsAge);
+// friendsAge.shift();
+// console.log(friendsAge);
 
 // console.log(friendsAge.splice(friendsAge.indexOf(0),1));
-friendsAge.push(2);
-friendsAge.push(25);
-friendsAge.push(26);
-friendsAge.push(27);
-friendsAge.push(28);
-friendsAge.push(29);
-console.log(friendsAge);
+// friendsAge.push(2);
+// friendsAge.push(25);
+// friendsAge.push(26);
+// friendsAge.push(27);
+// friendsAge.push(28);
+// friendsAge.push(29);
+// console.log(friendsAge);
 
-friendsAge.unshift(2);
-friendsAge.unshift(25);
-friendsAge.unshift(26);
-friendsAge.unshift(27);
-friendsAge.unshift(28);
-friendsAge.unshift(29);
-console.log(friendsAge);
+// friendsAge.unshift(2);
+// friendsAge.unshift(25);
+// friendsAge.unshift(26);
+// friendsAge.unshift(27);
+// friendsAge.unshift(28);
+// friendsAge.unshift(29);
+// console.log(friendsAge);
 
-console.log(friendsAge[28]);
+// console.log(friendsAge[28]);
 
 
 
