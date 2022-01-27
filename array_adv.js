2637f6c158d8b504966dc9982eb6a31329bb60d3
anamerasul <anamerasul@gmail.com>
2022-01-27 17:21:21 +0600

    push pop unshift shoift

---
+++ array_adv.js
@@ -0,0 +1,27 @@
+// array index
+
+// var bookpages =['hablu','bolod','chodugusti','battery','paracitamol','gym'];
+var books =['hablu','bolod','chodugusti','battery','paracitamol','gym'];
+
+// var batteryIndex=pages.indexOf('battery');
+
+// console.log(batteryIndex);
+
+var numbers= [45,98,64,78,87,64];
+
+console.log(numbers.indexOf(87));
+console.log(numbers.indexOf(7));
+
+
+var secondIndex= books[0];
+console.log(secondIndex);
+console.log(numbers);
+
+numbers[1]=95;
+numbers[4]=97;
+
+console.log(numbers);
+console.log(numbers[7]);
+
+// Add or remove element from array using push, pop
+
---
+++ push_pop.js
@@ -0,0 +1,48 @@
+var lastBench=['kalam','balam','salam'];
+console.log(lastBench[2]);
+console.log(lastBench.indexOf('jamal'));
+lastBench.push('jamal');
+lastBench.push('palam');
+lastBench.push('nalam');
+// console.log(lastBench[3]);
+
+// var index=lastBench.indexOf('jamal')
+
+// console.log(lastBench);
+
+// console.log(index);
+
+// add last element on arrary  
+
+var friendsAge=[11,13,17,22,23];
+// friendsAge.push(15);
+// console.log(friendsAge);
+
+// friendsAge.pop();
+// console.log(friendsAge);
+// friendsAge.pop();
+// friendsAge.pop();
+console.log(friendsAge);
+// friendsAge.push(23);
+// console.log(friendsAge);
+// friendsAge.pop();
+// console.log(friendsAge)
+
+var lastItem =friendsAge.pop();
+console.log(lastItem)
+
+// friendsAge.pop();
+// friendsAge.pop();
+// friendsAge.pop();
+// friendsAge.pop();
+// console.log(friendsAge);
+
+// unshift add element array from first
+friendsAge.unshift(15);
+console.log(friendsAge);
+// delete element from frist using shift() in arry
+friendsAge.shift();
+console.log(friendsAge);
+friendsAge.shift();
+console.log(friendsAge);
+
