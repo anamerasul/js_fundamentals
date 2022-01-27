28c4d66d522cc4175777243e381836cc45f487b7
anamerasul <anamerasul@gmail.com>
2022-01-27 15:05:29 +0600

    variable

---
+++ var.js
@@ -0,0 +1,18 @@
+var watchPrice = 120;
+watchPrice=125;
+
+console.log(typeof watchPrice);
+
+var oneNumber = 0.3;
+var twoNumber =0.4;
+
+var sum = twoNumber- oneNumber;
+
+console.log(sum.toFixed(2));
+console.log(typeof sum.toFixed(2));
+
+
+
+console.log(parseFloat(sum.toFixed(2)));
+console.log(typeof parseFloat(sum.toFixed(2)));
+
