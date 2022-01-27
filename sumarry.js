de37b2a6fdb2bdd14b6bb12512967727ce9656fc
anamerasul <anamerasul@gmail.com>
2022-01-27 21:10:39 +0600

    summery

---
+++ sumarry.js
@@ -0,0 +1,35 @@
+// var name = content ;
+ var tableLenth =12;
+ var travelDestination =['sunderbon','kuakata','coxbazar','nepal','paris'];
+
+var pos = travelDestination.indexOf('nepal');
+
+var forth = travelDestination[3];
+console.log(forth)
+console.log(travelDestination)
+travelDestination[1] ="srilamnka"
+console.log(travelDestination)
+
+// if(travelDestination[1]=='kuakata'){
+
+// }
+// if(travelDestination[1]=='kuakata'){
+
+// }
+// if(travelDestination[1]=='kuakata'){
+
+// }
+// if(travelDestination[1]=='kuakata'){
+
+// }
+// if(travelDestination[1]=='kuakata'){
+
+// }
+
+var eggPrice =32;
+var MyBugget =32;
+
+if (eggPrice<=MyBugget) {
+    console.log("khabo")
+    
+}
