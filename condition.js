4b4b117a64a984899236542bbd412d4e7544e426
anamerasul <anamerasul@gmail.com>
2022-01-27 20:05:54 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,46 @@
+var isFoodReady = false;
+
+// if food is ready I will eat
+// if food is ready ==true
+// if (condition){
+
+// }
+
+if (isFoodReady == true){
+    console.log('mama I will eat now')
+}
+
+else{
+    console.log("pore khabo")
+}
+
+var iphonePrice =70000;
+var myBudget =11000;
+
+// if (iphonePrice <myBudget){
+//     console.log("KINBO")
+// }
+
+// else{
+//     console.log('KINBO NA')
+// };
+
+// if(condition){
+//     // true hole run hbw
+// }
+
+// else{
+//     // false hole run hbe
+// }
+
+var chickenPrice =180;
+var burgerPrice =120;
+var myMoney = 850;
+
+if (chickenPrice < myMoney && burgerPrice<chickenPrice  && burgerPrice<myMoney){
+    console.log("yes murgi khabo")
+}
+
+else{
+    console.log("vorta vat")
+}
